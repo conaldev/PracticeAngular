1675f07725a99d0261a048ea0226addfa51b197c
conaldev <talaaidoaibiet@gmail.com>
2020-10-23 10:44:09 +0700

    input like

--- src/app/hacker-news/hacker-news.component.ts
+++ src/app/hacker-news/hacker-news.component.ts
@@ -9,34 +9,41 @@ export class HackerNewsComponent implements OnInit {
   articles = [
     {
       title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await',
-      url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40'
+      url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40',
+      like: 5
     },
     {
       title: 'Game of Life',
-      url: 'https://thefullsnack.com/posts/game-of-life.html'
+      url: 'https://thefullsnack.com/posts/game-of-life.html',
+      like: 6
     },
     {
       title: 'Nguyên tắc thiết kế REST API',
-      url: 'https://medium.com/eway/nguyên-tắc-thiết-kế-rest-api-23add16968d7'
+      url: 'https://medium.com/eway/nguyên-tắc-thiết-kế-rest-api-23add16968d7',
+      like: 7
     },
     {
       title: 'Why You Only Need to Test with 5 Users',
-      url: 'https://www.nngroup.com/articles/why-you-only-need-to-test-with-5-users/'
+      url: 'https://www.nngroup.com/articles/why-you-only-need-to-test-with-5-users/',
+      like: 8
     },
     {
       title: 'Let’s Build A Web Server. Part 1.',
-      url: 'https://ruslanspivak.com/lsbaws-part1/'
+      url: 'https://ruslanspivak.com/lsbaws-part1/',
+      like: 3
     }
   ];
   article = {
     title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await',
-    url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40'
+    url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40',
+    like: 2
   };
   addArticle(){
     this.articles.push({
       title : this.article.title,
-      url : this.article.url
-    })
+      url : this.article.url,
+      like: 0
+    });
   }
   constructor() { }
 
--- src/app/likes/likes.component.ts
+++ src/app/likes/likes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-likes',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./likes.component.scss']
 })
 export class LikesComponent implements OnInit {
-  likes: number = 0;
+  @Input() likes: number;
   // tslint:disable-next-line:typedef
   likeThis() {
     this.likes++;
