45376147a7740c552199b007ef708c4df158ce31
conaldev <talaaidoaibiet@gmail.com>
2020-10-26 21:22:36 +0700

    youtube playlist embed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import {NgModule} from '@angular/core';
-import {TimelinesComponent} from './timelines/timelines.component';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import { YoutubePlaylistComponent } from './youtube-playlist/youtube-playlist.component';
+import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TimelinesComponent
+    YoutubePlaylistComponent,
+    YoutubePlayerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
   ],
   bootstrap: [AppComponent]
 })
